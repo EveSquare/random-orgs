c494c4650dc5096f396377ca65cf2a31ccb9703d
EveSquare <50450074+EveSquare@users.noreply.github.com>
2022-12-07 23:04:39 +0900

    [add] 実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,110 @@
-import Hero from 'components/Hero'
+import { Box, Button } from "@chakra-ui/react";
 import Container from 'components/Container'
 import DarkModeSwitch from 'components/DarkModeSwitch'
+import OrganizationCard from "components/OrganizationCard";
+import { Organization } from "components/OrganizationCard/type";
+import { useEffect, useState } from "react";
 
-const Index = () => (
-  <Container height="100vh">
-    <DarkModeSwitch />
+const Index = () => {
 
-    <Hero />
-  </Container>
-)
+  const LIMIT = 10;
+  const [orgs, setOrgs] = useState<Organization[]>([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [buttonState, setButtonState] = useState({
+    prev: true,
+    next: false,
+  });
+
+  async function getOrgs() {
+    const result = await fetch('api/getRandomOrgs')
+      .then(response => response.json());
+    return result;
+  }
+
+  async function OnClickNext() {
+    const isLimit = LIMIT === orgs.length - 1;
+    const isLast = currentIndex === orgs.length - 1;
+
+    if (isLimit && isLast) {
+      setButtonState({
+        ...buttonState,
+        next: true,
+      });
+      return;
+    }
+
+    if (isLast) {
+      const newOrgs = [...orgs];
+      newOrgs.push(await getOrgs());
+      setOrgs(newOrgs);
+    }
+
+    setCurrentIndex(currentIndex + 1);
+    orgs.length > 1 && setButtonState({
+      ...buttonState,
+      prev: false,
+    });
+  }
+
+  function OnClickPrev() {
+    const isFirst = currentIndex === 0;
+    if (isFirst) {
+      setButtonState({
+        ...buttonState,
+        prev: true,
+      });
+      return;
+    }
+    setCurrentIndex(currentIndex - 1);
+    orgs.length > 1 && setButtonState({
+      ...buttonState,
+      next: false,
+    });
+  }
+
+  useEffect(() => {
+    async function fetchOrgs() {
+      const newOrgs = await getOrgs();
+      setOrgs([newOrgs]);
+    }
+    fetchOrgs();
+  }, []);
+
+  return (
+    <Container height="100vh">
+      <DarkModeSwitch />
+
+      <Box display="flex" justifyContent="center" alignItems="center" h="100vh" w={"60%"}>
+        <Box display="flex" flexDirection="column" minWidth={"100%"}>
+          {orgs[currentIndex] ?
+            <OrganizationCard
+              organization={orgs[currentIndex]}
+            />
+            :
+            <></>
+          }
+          <Box display="flex" justifyContent="center" m="3rem">
+            <Button
+              disabled={buttonState.prev}
+              onClick={OnClickPrev}
+              p="2rem"
+              m="2rem"
+              size='lg'
+              variant='outline'
+            >Prev</Button>
+            <Button
+              disabled={buttonState.next}
+              onClick={OnClickNext}
+              p="2rem"
+              m="2rem"
+              colorScheme='teal'
+              size='lg'
+            >{orgs.length == LIMIT ? "LIMIT" : "Feel the destiny"}</Button>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  )
+};
 
 export default Index
