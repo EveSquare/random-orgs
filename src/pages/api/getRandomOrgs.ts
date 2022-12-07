399f44eb5d17e3f2fe3c0be4e481d8d075e182a0
EveSquare <50450074+EveSquare@users.noreply.github.com>
2022-12-07 23:03:38 +0900

    [add] api

---
+++ src/pages/api/getRandomOrgs.ts
@@ -0,0 +1,24 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fetch from "node-fetch-commonjs";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const API_KEY = process.env.NEXT_PUBLIC_GITHUB_TOEKN;
+
+    // APIリクエストを送信
+    const response = await fetch("https://api.github.com/organizations",
+        {
+            headers: {
+                "Authorization": "token " + API_KEY,
+            }
+        }
+    );
+
+    // APIレスポンスを取得
+    const organizations: any = await response.json();
+
+    // ランダムにOrganizationを選択
+    const organization = organizations[Math.floor(Math.random() * organizations.length)];
+
+    res.status(200).json(organization || {});
+}
